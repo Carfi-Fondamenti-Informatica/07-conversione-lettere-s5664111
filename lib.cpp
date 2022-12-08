f23f9d3d131eb2f5fcc3eb1e64a6045901300e9e
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-08 19:15:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+bool funzione (char &a){
+  if ((a>64)&&(a<91)){
+    a=a+32;
+    return true;
+  }
+  else if((a>96)&&(a<123){
+    a=a-32;
+    return true;
+  }
+  else{
+    return false;
+  }
+          }
