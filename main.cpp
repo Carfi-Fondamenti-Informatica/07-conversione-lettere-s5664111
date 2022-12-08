d76900d59b36af63b004096eb78744a5a0706800
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-08 19:19:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char a=0;
+  cin>>a;
+  if(funzione(a)==1){
+    cout<<char(a)<<endl;
+  }
+  else{
+    cout<<"errore"<<endl;
+  }
   return 0;
 }
